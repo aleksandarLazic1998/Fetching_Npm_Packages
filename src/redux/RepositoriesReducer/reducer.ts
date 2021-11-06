71a1d917ff323f3b6daab783409f109604227453
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2021-11-06 14:18:07 +0100

    Sync React Search Functionality with typescript redux

--- src/redux/RepositoriesReducer/reducer.ts
+++ src/redux/RepositoriesReducer/reducer.ts
@@ -8,27 +8,27 @@ const initialState: IInitialSearchRepo = {
 	error: "",
 };
 
+/* eslint-disable default-case */
 const repositoriesReducer = (
 	state = initialState,
 	action: TRepostioriesAction
-) => {
+) =>
 	produce(state, (draft) => {
 		switch (action.type) {
 			case Repositories.GET_REPOSITORES:
 				draft.loading = true;
-				return draft;
+				break;
 
 			case Repositories.GET_REPOSITORES_SUCCESS:
 				draft.loading = false;
 				draft.data = action.payload;
-				return draft;
+				break;
 
 			case Repositories.GET_REPOSITORES_FAILURE:
 				draft.loading = false;
 				draft.error = action.payload;
-				return draft;
+				break;
 		}
 	});
-};
 
 export default repositoriesReducer;
